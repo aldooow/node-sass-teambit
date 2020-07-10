d9584a4ed5a4625f435f89a00af36736e48c8697
Aldo Cahuana <aldooow@icloud.com>
2020-07-10 23:38:00 +0200

    Work in progress!!

---
+++ dist/script.js
@@ -0,0 +1,10 @@
+$(document).ready(
+  function(){
+
+    $('.ms_menu-hamburger').click(function(){
+      $('.ms_menu').toggle();
+      $('.open').toggle();
+      $('.close').toggle();
+    });
+
+  });
